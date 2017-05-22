178292d7e4d9c1ae2473bdf0e93cbbc1d09981ca
章枫XX <83128773@qq.com>
2017-05-22 21:45:27 +0800

    2017年5月22日21:45:23

--- food/碰撞.cs
+++ food/碰撞.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class 碰撞 : MonoBehaviour {
-
 	// Use this for initialization
 	void Start () {
 		
@@ -15,7 +14,8 @@ public class 碰撞 : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D food) {
-		if (food.name == "player")
-			Destroy(gameObject);
+		if (food.name == "player") {
+			Destroy (gameObject);
+		}
 	}
 }
